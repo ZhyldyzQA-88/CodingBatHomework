ad3f8fea588edc5464ce7723f6bfee1f956a971f
Zhyldyz <zhyldyz9494@gmail.com>
2026-07-20 01:46:38 +0600

    MakeAbba

--- src/main/java/String1/MakeAbba.java
+++ src/main/java/String1/MakeAbba.java
@@ -1,14 +1,14 @@
 package String1;
 
-import java.util.Scanner;
-
 public class MakeAbba {
     public static void main(String[] args) {
-
+        System.out.println(makeAbba("Hi", "Bye"));
+        System.out.println(makeAbba("Yo", "Alice"));
 
     }
 
-    public String makeAbba (String a, String b) {
+    public static String makeAbba (String a, String b) {
+        return a + b + b + a;
 
     }
 }
