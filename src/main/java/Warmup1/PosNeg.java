4b2d9db389de3e2b6a9582f3d6a6b48bf98d2a40
Zhyldyz <zhyldyz9494@gmail.com>
2026-07-20 00:03:05 +0600

    POsNeg

---
+++ src/main/java/Warmup1/PosNeg.java
@@ -0,0 +1,21 @@
+package Warmup1;
+
+public class PosNeg {
+
+    public static boolean posNeg(int a, int b, boolean negative) {
+        if (negative && a < 0 && b < 0) {
+            return true;
+        }
+        if (!negative && ((a < 0 && b > 0) || (a > 0 && b < 0))) {
+            return true:
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(posNeg(1, -2, false));
+        System.out.println(posNeg(-3, 5, false));
+        System.out.println(posNeg(-8, -2, true));
+        System.out.println(posNeg(-1, -1, false));
+    }
+}
