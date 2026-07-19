46b176fa52270c0aa127bb8428337eb61c1d070e
Zhyldyz <zhyldyz9494@gmail.com>
2026-07-20 01:33:06 +0600

    StringSplosion

---
+++ src/main/java/String1/HelloName.java
@@ -0,0 +1,19 @@
+package String1;
+
+public class HelloName {
+    public static void main(String[] args) {
+
+    }
+
+    public static String helloName (String name) {
+        String result = "";
+
+        for (int i = 0; i < name.length(); i++) {
+            result = result +name.substring("Code")
+            result = result +name.substring("Developer")
+            result = result +name.substring("Mango")
+
+        }
+        return result;
+    }
+}
